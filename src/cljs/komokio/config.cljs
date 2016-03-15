5617098b8a2455d1bf9828d9d716387446c3e1c9
Sean Irby <sean.t.irby@gmail.com>
2016-03-15 12:46:54 -0700

    Add some more types to example code

--- src/cljs/komokio/config.cljs
+++ src/cljs/komokio/config.cljs
@@ -8,19 +8,47 @@
 
                        :face/foreground {:db/id 201
                                          :color/name "blue"
-                                         :color/rgb  "#1e90ff"}}
+                                         :color/rgb  "#1e90ff"}
 
+                       :face/background {:db/id 204
+                                         :color/name "black"
+                                         :color/rgb  "#1a1a1a"}}
                       {:db/id 101
+                       :face/name :keyword
+
+                       :face/foreground {:db/id 202
+                                         :color/name "red"
+                                         :color/rgb  "#Cd0000"}
+
+                       :face/background {:db/id 204
+                                         :color/name "black"
+                                         :color/rgb  "#1a1a1a"}}
+                      {:db/id 102
+                       :face/name :function
+
+                       :face/foreground {:db/id 200
+                                         :color/name "yellow"
+                                         :color/rgb  "Ffd700"}
+
+                       :face/background {:db/id 204
+                                         :color/name "black"
+                                         :color/rgb  "#1a1a1a"}}
+
+                      {:db/id 103
                        :face/name :comment
 
                        :face/foreground {:db/id 203
                                          :color/name "green"
-                                         :color/rgb  "#2e8b57"}}]
+                                         :color/rgb  "#2e8b57"}
+
+                       :face/background {:db/id 204
+                                         :color/name "black"
+                                         :color/rgb  "#1a1a1a"}}]
 
          ;; TODO rgb property name since they're not RGB values (hsv i think)
          :colors/list [{:db/id 204
                         :color/name "black"
-                        :color/rgb "#000000"}
+                        :color/rgb "#1a1a1a"}
 
                        {:db/id 200
                         :color/name "yellow"
@@ -40,5 +68,9 @@
 
 (def code-clojure "<pre><code><span class=\"code code-comment\">
   ;; this is a comment</span><span class=\"code code-default\">
-  This is some default text</span><span class=\"code code-default\">
-  This is even more default text</span></code></pre>")
+
+  This is some default text</span><span class=\"code code-function\">
+
+  This is what a function looks like</span><span class=\"code code-keyword\">
+
+  This is what a keyword looks like</span></span></code></pre>")
