3101aa0fe2493b163a625bf5457f3bfc54e73304
Sean Irby <sean.t.irby@gmail.com>
2016-03-17 08:31:50 -0700

    Establish listening channel for PalettePicker
    FaceColor components can now send signals to
    the PalettePicker.

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.8.0-RC5"]
+                 [org.clojure/core.async "0.2.374"]
                  [org.clojure/clojurescript "1.7.228"]
                  [org.clojure/tools.nrepl "0.2.12"]
                  [org.omcljs/om "1.0.0-alpha30"]
--- src/cljs/komokio/components/faceeditor.cljs
+++ src/cljs/komokio/components/faceeditor.cljs
@@ -1,10 +1,14 @@
 (ns komokio.components.faceeditor
+  (:require-macros [cljs.core.async.macros :refer [go go-loop]])
   (:require [goog.dom :as gdom]
             [om.next :as om :refer-macros [defui]]
             [om.dom :as dom]
+            [cljs.core.async :refer [timeout mult tap untap put! chan <! >!]]
 
             [komokio.components.palette :refer [Color]]
             [komokio.util :as util]))
+
+(def palette-picker-chan (chan))
 
 (defn update-face-color-style [comp]
   (let [props (om/props comp)
@@ -32,12 +36,23 @@
 
 (defui PalettePicker
   Object
+  (componentWillMount [this]
+    (let [ch (:palette-picker-chan (om/get-computed this))]
+      (go-loop [f (<! ch)]
+        (println "signal received")
+        (recur (<! ch)))))
+
   (render [this]
-    (let [color-options (om/props this)]
+    (let [color-options (:color-options (om/props this))]
       (apply dom/div #js {:className"palette-picker"}
         (map color-option color-options)))))
 
 (def palette-picker (om/factory PalettePicker))
+
+
+(defn faceColorClick [e comp]
+  (let [editing? (:editing? (om/props comp))]
+    (om/update-state! comp assoc :editing? (not editing?))))
 
 (defui FaceColor
   static om/Ident
@@ -51,13 +66,17 @@
 
   Object
   (componentDidUpdate [this prev-props prev-state]
-    (update-face-color-style this))
-
+    ;; TODO would be better to have dedicated components in code display to render face styles
+    (update-face-color-style this)
+    (let [{:keys [editing?] :as state} (om/get-state this)
+          {ch :palette-picker-chan} (om/get-computed this)]
+      (when editing? (put! ch comp))))
   (render [this]
-    (let [{:keys [color/rgb] :as props} (om/props this)]
+    (let [{:keys [color/rgb] :as props} (om/props this)
+          {:keys [editing?] :as state} (om/get-state this)]
       (dom/div #js {:style #js {:backgroundColor rgb}
                     :className "color color-trigger"
-                    :onClick #(println "showing palette picker")} ""))))
+                    :onClick #(faceColorClick % this)} ""))))
 
 (def face-color (om/factory FaceColor))
 
@@ -80,20 +99,21 @@
            bg :face/background
            fg :face/foreground
            :as props} (om/props this)
-
+          computed (om/get-computed this)
           cssUpdater (fn [css-property color-rgb]
                        (util/update-code-css face-name css-property color-rgb))
 
           bg-computed (om/computed
                         bg
-                        {:css-updater (partial cssUpdater "background-color")})
+                        (merge computed
+                          {:css-updater (partial cssUpdater "background-color")}))
 
           fg-computed (om/computed
                         fg
-                        {:css-updater (partial cssUpdater "color")})]
+                        (merge computed
+                          {:css-updater (partial cssUpdater "color")}))]
       (dom/div #js {:className "face-container"}
         (dom/div #js {:className "face"}
-
           (dom/span #js {:className "face-name"} (clojure.core/name face-name))
           (face-color fg-computed)
           (face-color bg-computed))))))
@@ -103,12 +123,15 @@
 (defui FaceEditor
   Object
   (render [this]
-    (let [props (om/props this)
-          {color-options :color-options/list faces :faces/list} props]
+    (let [{color-options :color-options/list
+           faces :faces/list :as props} (om/props this)
+
+          faces-computed         (map #(om/computed % {:palette-picker-chan palette-picker-chan}) faces)
+          color-options-computed (om/computed {:color-options color-options} {:palette-picker-chan palette-picker-chan})]
       (dom/div #js {:id "faces-editor"
                     :className "widget"}
         (dom/h3 nil "Faces Editor")
-        (apply dom/div nil (map face faces))
-        (palette-picker color-options)))))
+        (apply dom/div nil (map face faces-computed))
+        (palette-picker color-options-computed)))))
 
 (def face-editor (om/factory FaceEditor))
