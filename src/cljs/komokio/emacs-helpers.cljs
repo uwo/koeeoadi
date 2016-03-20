4c2509dbb3de3d809956522523b84cfdbfcf89fb
seanirby <sean.t.irby@gmail.com>
2016-03-20 12:36:11 -0700

    Add some helpers for generating code-data

---
+++ src/cljs/komokio/emacs-helpers.cljs
@@ -0,0 +1,15 @@
+(ns komokio.emacs-helpers)
+
+(defn plist-to-map [plist]
+  (let [plist-to-map' (fn [x]
+                        (if (coll? x)
+                          (into {} (mapv #(into [] %) (partition 2 2 x)))
+                          x))]
+
+    (mapv plist-to-map' plist)))
+
+
+
+
+
+
